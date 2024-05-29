4dddff2f42da5d2242f745d9ab1c4de2938457fb
Clara <clarafdezfraga@gmail.com>
2024-05-29 11:47:36 +0200

    Create PicturesController with getPictures method

---
+++ src/picture/controller/PicturesController.ts
@@ -0,0 +1,18 @@
+import { type Request, type Response } from "express";
+import { type PictureRepositoryStructure } from "../repository/types";
+import { type PicturesControllerStructure } from "./types";
+
+class PicturesController implements PicturesControllerStructure {
+  constructor(private readonly picuresRepository: PictureRepositoryStructure) {}
+
+  async getPictures(_req: Request, res: Response): Promise<void> {
+    try {
+      const pictures = await this.picuresRepository.getAll();
+      res.status(200).json({ pictures });
+    } catch (error) {
+      res.status(404).json({ error: "failed to load Pictures" });
+    }
+  }
+}
+
+export default PicturesController;
