1930055c3949c179cdf71890690cbcba74b343d0
Clara <clarafdezfraga@gmail.com>
2024-06-01 15:14:42 +0200

    Configure cors

--- src/server/app/app.ts
+++ src/server/app/app.ts
@@ -3,10 +3,12 @@ import artworkRouter from "../../artwork/router/artworkRouter.js";
 import notFoundError from "../middlewares/notFoundError.js";
 import { generalError } from "../middlewares/generalError.js";
 import routes from "../routes/routes.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
-app.use(routes.artworksRoute, artworkRouter);
+app.use(routes.artworks, artworkRouter);
 
 app.use(notFoundError);
 app.use(generalError);
