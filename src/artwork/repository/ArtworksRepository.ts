6c52c164966a2f11a0d1a756eeb4238e503ad790
Clara <clarafdezfraga@gmail.com>
2024-06-08 22:49:05 +0200

    Add createArtwork to artworksMongooseRepository

--- src/artwork/repository/ArtworksRepository.ts
+++ src/artwork/repository/ArtworksRepository.ts
@@ -1,11 +1,13 @@
 import { type Model } from "mongoose";
-import { type ArtworksRepository } from "./types.js";
+import { type ArtworkData, type ArtworksRepository } from "./types.js";
 import type ArtworkStructure from "../types.js";
 
 class ArtworksMongooseRepository implements ArtworksRepository {
   constructor(public readonly model: Model<ArtworkStructure>) {}
 
   getAll = async (): Promise<ArtworkStructure[]> => this.model.find().exec();
+  createArtwork = async (artworkData: ArtworkData): Promise<ArtworkStructure> =>
+    this.model.create(artworkData);
 }
 
 export default ArtworksMongooseRepository;
--- src/artwork/repository/types.ts
+++ src/artwork/repository/types.ts
@@ -2,4 +2,7 @@ import type ArtworkStructure from "../types.js";
 
 export interface ArtworksRepository {
   getAll(): Promise<ArtworkStructure[]>;
+  createArtwork(artowrkData: ArtworkData): Promise<ArtworkStructure>;
 }
+
+export type ArtworkData = Omit<ArtworkStructure, "_id" | "isFavourite">;
